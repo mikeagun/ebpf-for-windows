6a742941ed62a5f4cf092fcacf3ce332217b3993
Michael Agun <danielagun@microsoft.com>
2025-03-04 08:53:41 -0800

    Add generated perf array bind monitor bpf programs.

--- tests/bpf2c_tests/expected/bindmonitor_perf_event_array_dll.c
+++ tests/bpf2c_tests/expected/bindmonitor_perf_event_array_dll.c
@@ -77,7 +77,10 @@ _get_global_variable_sections(
 }
 
 static helper_function_entry_t bind_monitor_helpers[] = {
+<<<<<<< HEAD
     {6, "helper_id_6"},
+=======
+>>>>>>> 1da24ed0 (Add generated perf array bind monitor bpf programs.)
     {32, "helper_id_32"},
 };
 
@@ -92,6 +95,7 @@ static uint16_t bind_monitor_maps[] = {
 #pragma code_seg(push, "bind")
 static uint64_t
 bind_monitor(void* context, const program_runtime_context_t* runtime_context)
+<<<<<<< HEAD
 #line 24 "sample/bindmonitor_perf_event_array.c"
 {
 #line 24 "sample/bindmonitor_perf_event_array.c"
@@ -151,10 +155,58 @@ bind_monitor(void* context, const program_runtime_context_t* runtime_context)
     // EBPF_OP_JNE_IMM pc=6 dst=r1 src=r0 offset=19 imm=0
 #line 29 "sample/bindmonitor_perf_event_array.c"
     if (r1 != IMMEDIATE(0)) {
+=======
+#line 27 "sample/bindmonitor_perf_event_array.c"
+{
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    // Prologue.
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r0 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r1 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r2 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r3 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r4 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r5 = 0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    register uint64_t r10 = 0;
+
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    r1 = (uintptr_t)context;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
+
+    // EBPF_OP_LDXW pc=0 dst=r2 src=r1 offset=44 imm=0
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    r2 = *(uint32_t*)(uintptr_t)(r1 + OFFSET(44));
+    // EBPF_OP_JNE_IMM pc=1 dst=r2 src=r0 offset=9 imm=0
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    if (r2 != IMMEDIATE(0)) {
+#line 27 "sample/bindmonitor_perf_event_array.c"
+        goto label_1;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+    }
+    // EBPF_OP_LDXDW pc=2 dst=r4 src=r1 offset=0 imm=0
+#line 29 "sample/bindmonitor_perf_event_array.c"
+    r4 = *(uint64_t*)(uintptr_t)(r1 + OFFSET(0));
+    // EBPF_OP_LDXDW pc=3 dst=r5 src=r1 offset=8 imm=0
+#line 29 "sample/bindmonitor_perf_event_array.c"
+    r5 = *(uint64_t*)(uintptr_t)(r1 + OFFSET(8));
+    // EBPF_OP_JGE_REG pc=4 dst=r4 src=r5 offset=6 imm=0
+#line 29 "sample/bindmonitor_perf_event_array.c"
+    if (r4 >= r5) {
+>>>>>>> 1da24ed0 (Add generated perf array bind monitor bpf programs.)
 #line 29 "sample/bindmonitor_perf_event_array.c"
         goto label_1;
 #line 29 "sample/bindmonitor_perf_event_array.c"
     }
+<<<<<<< HEAD
     // EBPF_OP_LDXDW pc=7 dst=r1 src=r6 offset=8 imm=0
 #line 31 "sample/bindmonitor_perf_event_array.c"
     r1 = *(uint64_t*)(uintptr_t)(r6 + OFFSET(8));
@@ -221,6 +273,34 @@ label_1:
 #line 39 "sample/bindmonitor_perf_event_array.c"
     return r0;
 #line 24 "sample/bindmonitor_perf_event_array.c"
+=======
+    // EBPF_OP_SUB64_REG pc=5 dst=r5 src=r4 offset=0 imm=0
+#line 31 "sample/bindmonitor_perf_event_array.c"
+    r5 -= r4;
+    // EBPF_OP_LDDW pc=6 dst=r2 src=r1 offset=0 imm=1
+#line 30 "sample/bindmonitor_perf_event_array.c"
+    r2 = POINTER(runtime_context->map_data[0].address);
+    // EBPF_OP_LDDW pc=8 dst=r3 src=r0 offset=0 imm=-1
+#line 30 "sample/bindmonitor_perf_event_array.c"
+    r3 = (uint64_t)4294967295;
+    // EBPF_OP_CALL pc=10 dst=r0 src=r0 offset=0 imm=32
+#line 30 "sample/bindmonitor_perf_event_array.c"
+    r0 = runtime_context->helper_data[0].address(r1, r2, r3, r4, r5, context);
+#line 30 "sample/bindmonitor_perf_event_array.c"
+    if ((runtime_context->helper_data[0].tail_call) && (r0 == 0)) {
+#line 30 "sample/bindmonitor_perf_event_array.c"
+        return 0;
+#line 30 "sample/bindmonitor_perf_event_array.c"
+    }
+label_1:
+    // EBPF_OP_MOV64_IMM pc=11 dst=r0 src=r0 offset=0 imm=0
+#line 38 "sample/bindmonitor_perf_event_array.c"
+    r0 = IMMEDIATE(0);
+    // EBPF_OP_EXIT pc=12 dst=r0 src=r0 offset=0 imm=0
+#line 38 "sample/bindmonitor_perf_event_array.c"
+    return r0;
+#line 27 "sample/bindmonitor_perf_event_array.c"
+>>>>>>> 1da24ed0 (Add generated perf array bind monitor bpf programs.)
 }
 #pragma code_seg(pop)
 #line __LINE__ __FILE__
@@ -236,8 +316,13 @@ static program_entry_t _programs[] = {
         bind_monitor_maps,
         1,
         bind_monitor_helpers,
+<<<<<<< HEAD
         2,
         28,
+=======
+        1,
+        13,
+>>>>>>> 1da24ed0 (Add generated perf array bind monitor bpf programs.)
         &bind_monitor_program_type_guid,
         &bind_monitor_attach_type_guid,
     },
